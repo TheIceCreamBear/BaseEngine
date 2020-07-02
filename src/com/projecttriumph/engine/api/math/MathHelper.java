5111c5e4a2aede5ab8c4774f5784da06423449fd
Joseph <kartingman25@gmail.com>
2020-07-01 21:48:55 -0400

    Add angle methods to MathHelper

--- src/com/projecttriumph/engine/api/math/MathHelper.java
+++ src/com/projecttriumph/engine/api/math/MathHelper.java
@@ -1,5 +1,7 @@
 package com.projecttriumph.engine.api.math;
 
+import java.awt.geom.Point2D;
+
 public class MathHelper {
 	/**
 	 * Checks to see if the two number are equal within the given error margin
@@ -11,5 +13,27 @@ public class MathHelper {
 	public static boolean equal(double a, double b, double error) {
 		double ab = Math.abs(a - b);
 		return ab <= error;
+	}
+	
+	/**
+	 * Finds the angle in radians above the horizontal that the line these two points form from the starting object
+	 * to the target.
+	 * @param object - the calling object
+	 * @param target - the target object to find the able to
+	 * @return the angle in radians from the starting object to the target
+	 */
+	public static double getAngleRad(Point2D object, Point2D target) {
+		return Math.atan2(target.getY() - object.getY(), target.getX() - object.getX());
+	}
+	
+	/**
+	 * Finds the angle in degrees above the horizontal that the line these two points form from the starting object
+	 * to the target.
+	 * @param object - the calling object
+	 * @param target - the target object to find the able to
+	 * @return the angle in degrees from the starting object to the target
+	 */
+	public static double getAngle(Point2D object, Point2D target) {
+		return Math.toDegrees(getAngleRad(object, target));
 	}
 }
